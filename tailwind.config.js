bc010048e988b0f9a842c7b29773f7b056756cd1
Sergey Zakharevich <wobqqq@gmail.com>
2021-04-02 11:58:26 +0300

    Test bouncer

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,15 @@ module.exports = {
       }
     }
   },
-  variants: {},
+  variants: {
+    extend: {
+      backgroundColor: ['active', 'disabled'],
+      ringWidth: ['focus-visible'],
+      ringColor: ['hover', 'active', 'focus', 'focus-visible'],
+      ringOffsetWidth: ['responsive', 'focus-visible', 'focus'],
+      textColor: ['disabled'],
+    }
+  },
   plugins: [
     require('@tailwindcss/typography'),
     require('tailwindcss-pseudo-elements'),
