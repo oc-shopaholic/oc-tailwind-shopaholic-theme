f223eb8fbfbc24361634c1d62a50aef87b9aa766
Alexey Yakimov <alex@tumedia.ru>
2021-04-05 15:00:33 +0500

    Add hover, active, focus state for close button

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,18 @@ module.exports = {
         },
       gridTemplateRows: {
         'auto-fr-auto': 'auto 1fr auto',
+      },
+      outline: {
+        blue: ['1px dashed #1E40AF', '1px'],
       }
     }
   },
-  variants: {},
+  variants: {
+    extend: {
+      textColor: ['active', 'focus-visible'],
+      outline: ['focus-visible'],
+    }
+  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
