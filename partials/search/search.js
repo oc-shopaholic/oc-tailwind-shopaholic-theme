c91ccc912dcd3a2181b4f8038834fbc9abd1f328
Nik101 <nik101skryto@gmail.com>
2022-02-06 15:32:04 +0400

    Change the behavior of the delete button

--- partials/search/search.js
+++ partials/search/search.js
@@ -4,14 +4,19 @@ export default class Search {
   constructor(app) {
     this.$vNav = app;
     this.$vShow = this.$vNav.find("._show");
+    this.$sInput = null;
+    this.$sClear = null;
   }
 
   initEvents() {
     let app = this;
+    this.$sInput = this.$vNav.find("._shopaholic-search-input");
+    this.$sClear = this.$vNav.find("._clear");
     this.$vNav.on('click', '._clear', function () {
       const searchResultWrapper = $(app.$vNav.find(".search-result-wrapper"));
       const childrenNode = searchResultWrapper.children();
-      $(app.$vNav.find("._shopaholic-search-input")).val('');
+      $(app.$sInput.val(''));
+      app.$sClear.css('display', 'none')
       childrenNode.remove();
     })
   }
@@ -20,15 +25,25 @@ export default class Search {
     const obHelper = new ShopaholicSearch();
     obHelper.setAjaxRequestCallback(function (obRequestData) {
       obRequestData.update = { 'search/search-result': '.search-result-wrapper' };
-
       return obRequestData;
     }).init();
   }
 
+  initButtonClear(){
+    this.$sInput.on("input", (ev) => {
+      if($(ev.target).val()){
+        this.$sClear.css('display', 'block')
+      }else{
+        this.$sClear.css('display', 'none')
+      }
+    });
+  }
+
   show() {
     this.$vShow.on("click", () => {
-      this.initSearch()
-      this.initEvents()
+      this.initSearch();
+      this.initEvents();
+      this.initButtonClear();
     })
   }
 
