a52aba741197f660e6e4ab21a18f46a8b39fa79d
Nik101 <nik101skryto@gmail.com>
2022-03-03 12:44:46 +0400

    Moved 'RecentlyElem' to a separate object

--- partials/search/search.js
+++ partials/search/search.js
@@ -1,4 +1,5 @@
 import ShopaholicSearch from "@lovata/shopaholic-search";
+import RecentlyElem from "./recently-elem";
 
 export default class Search {
   constructor(app) {
@@ -427,62 +428,3 @@ export default class Search {
   }
 }
 Search.make('._off-canvas');
-
-export class RecentlyElem {
-  constructor(app) {
-    this.$vNav = app;
-    this.$vClear = this.$vNav.find('._clear-recently');
-    this.$vText = this.$vNav.find('._recently-text');
-
-    this.vHeader = $("._recently-container ._recently-header");
-    this.vInput = $("._shopaholic-search-input");
-    this.vClearAll = $("._clear-recently-all");
-  }
-
-  clear() {
-    this.$vClear.on("click", () => {
-      let history = JSON.parse(localStorage.searchHistory);
-      history = history.filter((item) => {
-        return item !== this.$vText.text()
-      })
-      if (!history.length) {
-        this.vHeader.css('display', 'none');
-      } else if (history.length <= 1) {
-        this.vClearAll.css('display', 'none');
-      }
-      let finalHistory = JSON.stringify(history);
-      localStorage.searchHistory = finalHistory;
-      this.$vNav.remove();
-    })
-  }
-
-  trigger() {
-    this.$vNav.on("click", () => {
-      this.vInput.val(this.$vText.text()).trigger('input');
-    })
-  }
-
-  behaviorEmulation() {
-    this.$vNav.on('keypress', function (e) {
-      if (e.which === 13 && e.target.classList[0] === '_recently') {
-        this.$vNav.trigger('click');
-      } else if (e.which === 13 && e.target.classList[0] === '_clear-recently') {
-        this.$vClear.trigger('click');
-      }
-    });
-  }
-
-  show() {
-    this.clear();
-    this.trigger();
-    this.behaviorEmulation();
-  }
-
-  static make(container) {
-    $(container).each(function (e) {
-      const containerNav = new RecentlyElem($(this));
-      containerNav.show();
-    });
-  }
-}
-
