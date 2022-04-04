63de793d26604e78c482e9f47d78bb183b41838b
Andrey Serdyuk <a.serdjuk@lovata.com>
2022-04-04 17:21:42 +0700

    Review Card block edits. p.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   corePlugins: {
@@ -22,18 +22,18 @@ module.exports = {
         blue: ['1px dashed #1E40AF', '1px'],
       },
       backgroundSize: {
-        '65': '65%',
+        65: '65%',
         'info-icon': '12.5rem',
       },
       fontSize: {
         error: ['9rem', '1'],
       },
       padding: {
-        '54': '13.5rem',
-        '57': '14.25rem',
+        54: '13.5rem',
+        57: '14.25rem',
       },
       maxHeight: {
-        '160': '40rem',
+        160: '40rem',
       },
       maxWidth: {
         'screen-3xl': '1920px'
@@ -51,7 +51,7 @@ module.exports = {
         '.separator': {
           content: 'url(/themes/lovata-tailwind-shopaholic/assets/images/separator.svg)',
         }
-      }
+      };
       addUtilities(newUtilities, ['before'])
     }),
     plugin(({ addVariant, e }) => {
@@ -60,4 +60,4 @@ module.exports = {
       })
     }),
   ]
-}
+};
