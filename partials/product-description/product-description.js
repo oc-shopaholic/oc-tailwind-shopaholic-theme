44da813c1ff754a1c66b7618edfa2e8503572edb
Nikita Konnov <n.konnov@lovata.com>
2022-04-18 22:52:31 +0400

    Replace binding to tags with classes

--- partials/product-description/product-description.js
+++ partials/product-description/product-description.js
@@ -1,16 +1,16 @@
 export default new class ProductDescription {
   constructor() {
-    this.vDescription = $("._description")
-    this.vButton = $("._description-toggle")
-    this.vSvg = this.vButton.children("svg")
-    this.vSpan = this.vButton.children("span"),
+    this.vDescription = $("._description");
+    this.vButton = $("._description-toggle");
+    this.vSvg = this.vButton.find("._arrow");
+    this.vSpan = this.vButton.find("._status");
     this.changeVisiblyDescription();
   }
 
   changeVisibly(text){
     this.vButton.css('opacity', '0');
     setTimeout(()=>{
-      this.vSpan.text(text)
+      this.vSpan.text(text);
       this.vButton.css('opacity', '1');
     },600)
   }
@@ -22,7 +22,6 @@ export default new class ProductDescription {
         this.vSvg.css('transform', 'rotate(0deg)');
         this.changeVisibly(window.stateButton.show);
       }else {
-        console.log(this.vDescription.css('-webkit-line-clamp'))
         this.changeVisibly(window.stateButton.hide);
         this.vDescription.css('-webkit-line-clamp', 'unset');
         this.vSvg.css('transform', 'rotate(180deg)');
