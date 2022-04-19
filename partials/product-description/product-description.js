6c038146ec0f531cbb21302bc7dbb1da2afcaf83
Nikita Konnov <n.konnov@lovata.com>
2022-04-19 17:40:57 +0400

    Add minimum amount of text for button output

--- partials/product-description/product-description.js
+++ partials/product-description/product-description.js
@@ -4,7 +4,16 @@ export default new class ProductDescription {
     this.vButton = $("._description-toggle");
     this.vSvg = this.vButton.find("._arrow");
     this.vSpan = this.vButton.find("._status");
+    this.vDescriptionText = this.vDescription.find("._description-text");
+
+    this.btnInit();
     this.changeVisiblyDescription();
+  }
+
+  btnInit(){
+    if(this.vDescriptionText[0] && this.vDescriptionText[0].childElementCount < 3){
+      this.vButton.css('display', 'none');
+    }
   }
 
   changeVisibly(text){
@@ -17,14 +26,16 @@ export default new class ProductDescription {
 
   changeVisiblyDescription(){
     this.vButton.on('click', ()=>{
-      if(this.vDescription.css('-webkit-line-clamp') !== '3'){
+      if(this.vButton.attr('aria-expanded') !== 'false'){
         this.vDescription.css('-webkit-line-clamp', '3');
         this.vSvg.css('transform', 'rotate(0deg)');
         this.changeVisibly(window.stateButton.show);
+        this.vButton.attr('aria-expanded', 'false');
       }else {
         this.changeVisibly(window.stateButton.hide);
         this.vDescription.css('-webkit-line-clamp', 'unset');
         this.vSvg.css('transform', 'rotate(180deg)');
+        this.vButton.attr('aria-expanded', 'true');
       }
     })
   }
