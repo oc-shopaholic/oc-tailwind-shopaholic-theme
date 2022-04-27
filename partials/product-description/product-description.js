8c8deddc148d0857dc86c5b9ce4f9dae9105fa89
Nikita Konnov <n.konnov@lovata.com>
2022-04-27 14:30:52 +0400

    Fix adding a class to tailwind

--- partials/product-description/product-description.js
+++ partials/product-description/product-description.js
@@ -16,45 +16,45 @@ export default new class ProductDescription {
 
   animInit(){
     this.sMinHeight = this.vDescriptionText.css('height');
-    this.vDescription.css('-webkit-line-clamp', 'unset');
+    this.vDescription.addClass('line-clamp-none');
     this.sMaxHeight = this.vDescriptionText.css('height');
-    this.vDescription.css('-webkit-line-clamp', '3');
+    this.vDescription.removeClass('line-clamp-none');
 
     this.vDescriptionText.css('max-height', this.sMinHeight);
   }
 
   btnInit(){
     if(this.vDescriptionText[0] && this.vDescriptionText[0].childElementCount < 3){
-      this.vButton.css('display', 'none');
+      this.vButton.addClass('hidden');
     }
   }
 
   changeVisibly(text){
-    this.vButton.css('opacity', '0');
+    this.vButton.addClass('opacity-0');
     setTimeout(()=>{
       this.vSpan.text(text);
-      this.vButton.css('opacity', '1');
+      this.vButton.removeClass('opacity-0');
     },200)
   }
 
   animClose(){
     this.vDescriptionText.css('max-height', this.sMinHeight);
     setTimeout(()=>{
-      this.vDescription.css('-webkit-line-clamp', '3');
+      this.vDescription.removeClass('line-clamp-none');
     },600)
   }
 
   changeVisiblyDescription(){
     this.vButton.on('click', ()=>{
       if(this.vButton.attr('aria-expanded') !== 'false'){
-        this.vSvg.css('transform', 'rotate(0deg)');
+        this.vSvg.removeClass('rotate-180');
         this.animClose();
         this.changeVisibly(window.stateButton.show);
         this.vButton.attr('aria-expanded', 'false');
       }else {
         this.changeVisibly(window.stateButton.hide);
-        this.vDescription.css('-webkit-line-clamp', 'unset');
-        this.vSvg.css('transform', 'rotate(180deg)');
+        this.vDescription.addClass('line-clamp-none');
+        this.vSvg.addClass('rotate-180');
         this.vButton.attr('aria-expanded', 'true');
         this.vDescriptionText.css('max-height', this.sMaxHeight);
       }
