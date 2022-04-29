46a71bb40045707e8aef98be370bca37d43e9662
Nikita Konnov <n.konnov@lovata.com>
2022-04-29 16:51:38 +0400

    Fix name with Hungarian notation

--- partials/product-description/product-description.js
+++ partials/product-description/product-description.js
@@ -1,10 +1,10 @@
 export default new class ProductDescription {
   constructor() {
-    this.vDescription = $("._description");
-    this.vButton = $("._description-toggle");
-    this.vSvg = this.vButton.find("._arrow");
-    this.vSpan = this.vButton.find("._status");
-    this.vDescriptionText = this.vDescription.find("._description-text");
+    this.obDescription = $("._description");
+    this.obButton = $("._description-toggle");
+    this.obSvg = this.obButton.find("._arrow");
+    this.obSpan = this.obButton.find("._status");
+    this.obDescriptionText = this.obDescription.find("._description-text");
 
     this.sMinHeight = '';
     this.sMaxHeight = '';
@@ -15,48 +15,48 @@ export default new class ProductDescription {
   }
 
   animInit(){
-    this.sMinHeight = this.vDescriptionText.css('height');
-    this.vDescription.addClass('line-clamp-none');
-    this.sMaxHeight = this.vDescriptionText.css('height');
-    this.vDescription.removeClass('line-clamp-none');
+    this.sMinHeight = this.obDescriptionText.css('height');
+    this.obDescription.addClass('line-clamp-none');
+    this.sMaxHeight = this.obDescriptionText.css('height');
+    this.obDescription.removeClass('line-clamp-none');
 
-    this.vDescriptionText.css('max-height', this.sMinHeight);
+    this.obDescriptionText.css('max-height', this.sMinHeight);
   }
 
   // btnInit(){
-  //   if(this.vDescriptionText[0] && this.vDescriptionText[0].childElementCount < 3){
-  //     this.vButton.addClass('hidden');
+  //   if(this.obDescriptionText[0] && this.obDescriptionText[0].childElementCount < 3){
+  //     this.obButton.addClass('hidden');
   //   }
   // }
 
   changeVisibly(text){
-    this.vButton.addClass('opacity-0');
+    this.obButton.addClass('opacity-0');
     setTimeout(()=>{
-      this.vSpan.text(text);
-      this.vButton.removeClass('opacity-0');
+      this.obSpan.text(text);
+      this.obButton.removeClass('opacity-0');
     },200)
   }
 
   animClose(){
-    this.vDescriptionText.css('max-height', this.sMinHeight);
+    this.obDescriptionText.css('max-height', this.sMinHeight);
     setTimeout(()=>{
-      this.vDescription.removeClass('line-clamp-none');
+      this.obDescription.removeClass('line-clamp-none');
     },600)
   }
 
   changeVisiblyDescription(){
-    this.vButton.on('click', ()=>{
-      if(this.vButton.attr('aria-expanded') !== 'false'){
-        this.vSvg.removeClass('rotate-180');
+    this.obButton.on('click', ()=>{
+      if(this.obButton.attr('aria-expanded') !== 'false'){
+        this.obSvg.removeClass('rotate-180');
         this.animClose();
         this.changeVisibly(window.stateButton.show);
-        this.vButton.attr('aria-expanded', 'false');
+        this.obButton.attr('aria-expanded', 'false');
       }else {
         this.changeVisibly(window.stateButton.hide);
-        this.vDescription.addClass('line-clamp-none');
-        this.vSvg.addClass('rotate-180');
-        this.vButton.attr('aria-expanded', 'true');
-        this.vDescriptionText.css('max-height', this.sMaxHeight);
+        this.obDescription.addClass('line-clamp-none');
+        this.obSvg.addClass('rotate-180');
+        this.obButton.attr('aria-expanded', 'true');
+        this.obDescriptionText.css('max-height', this.sMaxHeight);
       }
     })
   }
