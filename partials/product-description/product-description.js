10e7acf20fbb5c6c90cd9f22edad644ffd024e9e
Nikita Konnov <n.konnov@lovata.com>
2022-04-29 17:34:49 +0400

    Fix properties manipulation in style

--- partials/product-description/product-description.js
+++ partials/product-description/product-description.js
@@ -20,7 +20,7 @@ export default new class ProductDescription {
     this.sMaxHeight = this.obDescriptionText.css('height');
     this.obDescription.removeClass('line-clamp-none');
 
-    this.obDescriptionText.css('max-height', this.sMinHeight);
+    this.obDescriptionText.css('--description-text-max-height', this.sMinHeight);
   }
 
   btnInit(){
@@ -41,7 +41,7 @@ export default new class ProductDescription {
   }
 
   animClose(){
-    this.obDescriptionText.css('max-height', this.sMinHeight);
+    this.obDescriptionText.css('--description-text-max-height', this.sMinHeight);
     setTimeout(()=>{
       this.obDescription.removeClass('line-clamp-none');
     },600)
@@ -59,7 +59,7 @@ export default new class ProductDescription {
         this.obDescription.addClass('line-clamp-none');
         this.obSvg.addClass('rotate-180');
         this.obButton.attr('aria-expanded', 'true');
-        this.obDescriptionText.css('max-height', this.sMaxHeight);
+        this.obDescriptionText.css('--description-text-max-height', this.sMaxHeight);
       }
     })
   }
