107cfd5e73cbc7cbb58a8d8763ffa600466dedbc
Nikita Konnov <n.konnov@lovata.com>
2022-05-16 13:46:29 +0400

    Improv and corrected the gallery

--- partials/product-gallery/product-gallery.js
+++ partials/product-gallery/product-gallery.js
@@ -2,14 +2,16 @@ import Swiper from 'swiper/bundle';
 
 export default new class ProductGallery {
   constructor() {
-    this.gallerySelector = 'gallery-top';
-    this.paginationSelector = 'gallery-thumbs';
+    this.sGallerySelector = 'gallery-top';
+    this.sPaginationSelector = 'gallery-thumbs';
+    this.obSwiperContainer = $('._swiper-container');
+    this.obSlide = this.obSwiperContainer.find('.swiper-slide');
 
     this.handler();
   }
 
   handler() {
-    const slider = document.querySelector(`.${this.gallerySelector}`);
+    const slider = document.querySelector(`.${this.sGallerySelector}`);
 
     if (!slider) {
       return;
@@ -21,20 +23,51 @@ export default new class ProductGallery {
   }
 
   init() {
-    const galleryThumbs = new Swiper(`.${this.paginationSelector}`, {
-      spaceBetween: 10,
-      slidesPerView: 8,
+    const galleryThumbs = new Swiper(`.${this.sPaginationSelector}`, {
+      spaceBetween: 2,
+      slidesPerView: 'auto',
       freeMode: true,
       watchSlidesVisibility: true,
       watchSlidesProgress: true,
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      a11y:{
+        enabled: false
+      },
+      breakpoints: {
+        768: {
+          spaceBetween: 8,
+        },
+      },
     });
-    const galleryTop = new Swiper(`.${this.gallerySelector}`, {
+    
+    const galleryTop = new Swiper(`.${this.sGallerySelector}`, {
       spaceBetween: 0,
       allowSlidePrev: true,
       thumbs: {
-        slideThumbActiveClass: "border border-blue-800",
+        slideThumbActiveClass: "outline outline-2 outline-blue-800",
         swiper: galleryThumbs,
+        autoScrollOffset: 1,
       },
     });
+  
+    this.activeSlide(galleryThumbs);
+  }
+
+  activeSlide(slider){
+    let oldSliderIndex = null;
+    let oldSliderText = null;
+
+    if(!slider.clickedIndex){
+      oldSliderIndex = 0;
+      oldSliderText = this.obSlide[0].ariaLabel;
+      this.obSlide[0].ariaLabel = window.slider.active + ' ' + this.obSlide[0].ariaLabel;
+    }
+    this.obSlide.on('click', ()=>{
+      this.obSlide[oldSliderIndex].ariaLabel = oldSliderText;
+      oldSliderText = this.obSlide[slider.clickedIndex].ariaLabel;
+      this.obSlide[slider.clickedIndex].ariaLabel = window.slider.active + ' ' + this.obSlide[slider.clickedIndex].ariaLabel;
+      oldSliderIndex = slider.clickedIndex;
+    })
   }
 }();
