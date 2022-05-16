cceaba3c20fd35c70e9f5974deaf508a4c7b8c01
Nikita Konnov <n.konnov@lovata.com>
2022-05-16 20:15:35 +0400

    Add translation, fixed code, fixed output of images

--- partials/product-item/customer-reviews.js
+++ partials/product-item/customer-reviews.js
@@ -1,10 +1,31 @@
-export default new class Description {
+export default new class CustomerReviews {
   constructor() {
-    $("#show-more-reviews").click(() => {
-      $event.preventDefault()
+    this.obLoadMore = $('._show-more-reviews');
+    this.obListWrapper = '_review-list';
+    
+    this.init();
+  }
+
+  init(){
+    this.obLoadMore.on("click", () => {
+      const iPage = parseInt(this.obLoadMore.attr('data-page'), 10);
+      const iNextPage = iPage + 1;
+      const iMaxPage = parseInt(this.obLoadMore.attr('data-max-page'), 10);
+      
+      this.sendAjax(iNextPage);
+
+      if (iNextPage >= iMaxPage) {
+        this.obLoadMore.remove();
+      } else {
+        this.obLoadMore.attr('data-page', iNextPage);
+      }
     });
-    $("#show-less-reviews").click(() => {
-      $event.preventDefault()
+  }
+
+  sendAjax(iNextPage) {
+    $.request('ProductData::onAjaxRequest', {
+      data: { page: iNextPage },
+      update: { 'product-item/customer-reviews-ajax': `@.${this.obListWrapper}` }
     });
   }
 }();
