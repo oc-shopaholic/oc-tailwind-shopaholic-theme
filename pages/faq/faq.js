bd3fea479fcf226f8a866446048d03bcd65dcf3c
antalikavasilkova <antalikavasilkova@gmail.com>
2024-01-10 12:15:26 +0300

    add animation faq and catalog

--- pages/faq/faq.js
+++ pages/faq/faq.js
@@ -1,1 +1,9 @@
-import '/partials/accordion/accordion';
+import { AccordionInit } from '/partials/accordion/accordion'
+export default new class Faq {
+  constructor () {
+    this.init()
+  }
+  init () {
+    AccordionInit()
+  }
+}();
--- partials/accordion/accordion.js
+++ partials/accordion/accordion.js
@@ -1,55 +1,55 @@
 export default class Accordion {
-  constructor(el) {
-    this.el = el;
-    this.summary = el.querySelector('.js-accordion-summary');
-    this.content = el.querySelector('.js-accordion-content');
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding = false;
-    this.summary.addEventListener('click', (e) => this.onClick(e));
+  constructor (el) {
+    this.el = el
+    this.summary = el.querySelector('.js-accordion-summary')
+    this.content = el.querySelector('.js-accordion-content')
+    this.animation = null
+    this.isClosing = false
+    this.isExpanding = false
+    this.summary.addEventListener('click', (e) => this.onClick(e))
   }
 
-  onClick(e) {
-    e.preventDefault();
-    this.el.style.overflow = 'hidden';
+  onClick (e) {
+    e.preventDefault()
+    this.el.style.overflow = 'hidden'
     if (this.isClosing || !this.el.open) {
-      this.open();
+      this.open()
     } else if (this.isExpanding || this.el.open) {
-      this.shrink();
+      this.shrink()
     }
   }
 
-  shrink() {
-    this.isClosing = true;
-    const startHeight = `${this.el.offsetHeight}px`;
-    const endHeight = `${this.summary.offsetHeight}px`;
+  shrink () {
+    this.isClosing = true
+    const startHeight = `${this.el.offsetHeight}px`
+    const endHeight = `${this.summary.offsetHeight}px`
     if (this.animation) {
-      this.animation.cancel();
+      this.animation.cancel()
     }
     this.animation = this.el.animate({
       height: [startHeight, endHeight]
     }, {
       duration: 400,
       easing: 'linear'
-    });
+    })
 
-    this.animation.onfinish = () => this.onAnimationFinish(false);
-    this.animation.oncancel = () => this.isClosing = false;
+    this.animation.onfinish = () => this.onAnimationFinish(false)
+    this.animation.oncancel = () => this.isClosing = false
   }
 
-  open() {
-    this.el.style.height = `${this.el.offsetHeight}px`;
-    this.el.open = true;
-    window.requestAnimationFrame(() => this.expand());
+  open () {
+    this.el.style.height = `${this.el.offsetHeight}px`
+    this.el.open = true
+    window.requestAnimationFrame(() => this.expand())
   }
 
-  expand() {
-    this.isExpanding = true;
-    const startHeight = `${this.el.offsetHeight}px`;
-    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+  expand () {
+    this.isExpanding = true
+    const startHeight = `${this.el.offsetHeight}px`
+    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`
 
     if (this.animation) {
-      this.animation.cancel();
+      this.animation.cancel()
     }
 
     this.animation = this.el.animate({
@@ -57,22 +57,28 @@ export default class Accordion {
     }, {
       duration: 400,
       easing: 'linear'
-    });
-    this.animation.onfinish = () => this.onAnimationFinish(true);
-    this.animation.oncancel = () => this.isExpanding = false;
+    })
+    this.animation.onfinish = () => this.onAnimationFinish(true)
+    this.animation.oncancel = () => this.isExpanding = false
   }
 
-  onAnimationFinish(open) {
-    this.el.open = open;
-    this.animation = null;
-    this.isClosing = false;
-    this.isExpanding = false;
-    this.el.style.height = this.el.style.overflow = '';
+  onAnimationFinish (open) {
+    this.el.open = open
+    this.animation = null
+    this.isClosing = false
+    this.isExpanding = false
+    this.el.style.height = this.el.style.overflow = ''
   }
 }
-document.querySelectorAll('.js-details').forEach((el) => {
-  new Accordion(el);
-});
+
+export function AccordionInit () {
+  const details = document.querySelectorAll('.js-details')
+  if (details) {
+    document.querySelectorAll('.js-details').forEach((el) => {
+      new Accordion(el)
+    })
+  }
+}
 
 
 
